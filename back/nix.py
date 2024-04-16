7f1d52d003ae659d9b554e9c04693f34b88f966f
teawd <52975098+TeaWhyDee@users.noreply.github.com>
2024-04-16 20:14:46 +0300

    refactor nix
    Refactored into a class
    Fixed ValidPaths
    Added get_package_closure_size

--- back/nix.py
+++ back/nix.py
@@ -6,158 +6,199 @@ import subprocess
 import sqlite3
 
 
-STORES_ROOT = "/nix_stores/"
+DEFAULT_STORES_ROOT = "/nix_stores/"
 NIX_DB_PATH = "nix/var/nix/db/db.sqlite"
 
 
-def _run_cmd(command, throw_on_fail: bool = True):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+class Nix:
+    def __init__(self, stores_root: str = DEFAULT_STORES_ROOT):
+        self.stores_root = stores_root
 
-    if process.returncode != 0 and throw_on_fail:
-        error_message = stderr.decode().strip()
-        raise Exception(f"Error during nix execution:\n{error_message}")
+        if not os.path.exists(self.stores_root):
+            os.makedirs(self.stores_root)
 
-    return process.returncode, stdout, stderr
+    def _run_cmd(self, command, throw_on_fail: bool = True):
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        stdout, stderr = process.communicate()
 
+        if process.returncode != 0 and throw_on_fail:
+            error_message = stderr.decode().strip()
+            raise Exception(f"Error during nix execution:\n{error_message}")
 
-def _get_store_paths(store_id: str):
-    store_path = get_store_path(store_id)
+        return process.returncode, stdout, stderr
 
-    command = ["nix", "--store", store_path, "path-info", "--all"]
-    _, stdout, _ = _run_cmd(command)
-    paths = stdout.decode().strip().split("\n")
+    def _get_store_paths(self, store_id: str):
+        store_path = self.get_store_path(store_id)
 
-    return paths
+        command = ["nix", "--store", store_path, "path-info", "--all"]
+        _, stdout, _ = self._run_cmd(command)
+        paths = stdout.decode().strip().split("\n")
 
+        return paths
 
-def _get_package_closure(store_id: str, package):
-    store_path = get_store_path(store_id)
+    def _get_package_closure(self, store_id: str, package):
+        store_path = self.get_store_path(store_id)
 
-    command = ["nix", "--store", store_path, "path-info", "--recursive", package]
-    _, stdout, _ = _run_cmd(command)
-    paths = stdout.decode().strip().split("\n")
+        command = [
+            "nix",
+            "--store",
+            store_path,
+            "path-info",
+            "--recursive",
+            package,
+        ]
+        _, stdout, _ = self._run_cmd(command)
+        paths = stdout.decode().strip().split("\n")
 
-    return paths
+        return paths
 
+    def get_ValidPaths(self, store_id: str):
+        """
+        Returns ValidPaths table from db.sqlite of a store
+        """
 
-def get_ValidPaths(store_id: str):
-    """
-    Returns ValidPaths table from db.sqlite of a store
-    """
-    connection = sqlite3.connect("db.sqlite")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM ValidPaths")
-    results = cursor.fetchall()
+        store_path = self.get_store_path(store_id)
+        db_path = os.path.join(store_path, NIX_DB_PATH)
 
-    return results
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM ValidPaths")
+        results = cursor.fetchall()
 
+        return results
 
-def get_store_path(store_name: str) -> str:
-    return os.path.abspath(os.path.join(STORES_ROOT, store_name))
+    def get_store_path(self, store_name: str) -> str:
+        return os.path.abspath(os.path.join(self.stores_root, store_name))
 
+    def add_store(self, store_name: str) -> str:
+        """
+        Creates a new directory for a new store.
+        Raises exception if store already exists.
+        @returns: A path to created store (store ID).
+        """
 
-def add_store(store_name: str) -> str:
-    """
-    Creates a new directory for a new store.
-    Raises exception if store already exists.
-    @returns: A path to created store (store ID).
-    """
+        directory_path = self.get_store_path(store_name)
 
-    directory_path = get_store_path(store_name)
+        if os.path.exists(directory_path):
+            raise Exception(
+                f"Store named {store_name} already exists in the filetree."
+            )
 
-    if os.path.exists(directory_path):
-        raise Exception(f"Store named {store_name} already exists in the filetree.")
+        # Create store directory
+        os.makedirs(directory_path)
 
-    # Create store directory
-    os.makedirs(directory_path)
+        return store_name
 
-    return store_name
+    def remove_store(self, store_id: str):
+        """
+        Expects sanitized input.
+        Removes a store directory.
+        """
 
+        directory_path = self.get_store_path(store_id)
 
-def remove_store(store_id: str):
-    """
-    Expects sanitized input.
-    Removes a store directory.
-    """
+        # Check that the dir to be deleted in a folder we manage.
+        # TODO: decide if keep this check.
+        abs_root = os.path.abspath(self.stores_root)
+        if os.path.commonpath([directory_path, abs_root]) != abs_root:
+            raise Exception("Attempted to delete something outside stores dir")
 
-    directory_path = get_store_path(store_id)
+        shutil.rmtree(directory_path)
 
-    # Check that we are deleting a directory we should (against malicious user)
-    abs_root = os.path.abspath(STORES_ROOT)
-    if os.path.commonpath([directory_path, abs_root]) != abs_root:
-        raise Exception("Attempted to delete something outside stores dir.")
+    def add_package_to_store(self, store_id: str, package_name: str):
+        """
+        Adds (builds) a package to a store.
+        """
 
-    shutil.rmtree(directory_path)
+        store_path = self.get_store_path(store_id)
+        command = [
+            "nix",
+            "--store",
+            store_path,
+            "build",
+            "--no-link",
+            package_name,
+        ]
 
+        self._run_cmd(command)
 
-def add_package_to_store(store_id: str, package_name: str):
-    """
-    Adds (builds) a package to a store.
-    """
+    def remove_package_from_store(self, store_id: str, package_name: str):
+        """
+        Deletes a package recursively (closure of it's path).
+        Throws an exception on nix error.
+        """
 
-    store_path = get_store_path(store_id)
-    command = ["nix", "--store", store_path, "build", "--no-link", package_name]
+        store_path = self.get_store_path(store_id)
+        command = [
+            "nix",
+            "--store",
+            store_path,
+            "store",
+            "delete",
+            "--ignore-liveness",
+            "--recursive",
+            package_name,
+        ]
 
-    _run_cmd(command)
+        self._run_cmd(command)
 
+    def check_package_exists(self, store_id: str, package_name: str) -> bool:
+        """
+        Checks if specified package exists in a store by invoking nix path-info.
+        """
 
-def remove_package_from_store(store_id: str, package_name: str):
-    """
-    Deletes a package recursively (closure of it's path).
-    Throws an exception on nix error.
-    """
+        store_path = self.get_store_path(store_id)
+        command = ["nix", "--store", store_path, "path-info", package_name]
+        ret, _, _ = self._run_cmd(command, throw_on_fail=False)
+        if ret == 0:
+            return True
+        else:
+            return False
 
-    store_path = get_store_path(store_id)
-    command = [
-        "nix",
-        "--store",
-        store_path,
-        "store",
-        "delete",
-        "--ignore-liveness",
-        "--recursive",
-        package_name,
-    ]
+    def get_package_closure_size(self, store_id: str, package: str) -> int:
+        """
+        Returns closure size in bytes invoking `nix path-info --closure-size`.
+        """
 
-    _run_cmd(command)
+        store_path = self.get_store_path(store_id)
 
+        command = [
+            "nix",
+            "--store",
+            store_path,
+            "path-info",
+            "--closure-size",
+            package,
+        ]
+        _, stdout, _ = self._run_cmd(command)
 
-def check_package_exists(store_id: str, package_name: str) -> bool:
-    """
-    Checks if specified package exists in a store by invoking nix path-info.
-    """
+        size = stdout.decode().strip().split("\t")[1]
+        return int(size)
 
-    store_path = get_store_path(store_id)
-    command = ["nix", "--store", store_path, "path-info", package_name]
-    ret, _, _ = _run_cmd(command, throw_on_fail=False)
-    if ret == 0:
-        return True
-    else:
-        return False
+    def get_difference_of_paths(
+        self, store_id1: str, store_id2: str
+    ) -> List[str]:
+        """
+        Returns a list of paths that are in first store but not in second.
+        Throws an exception on nix error.
+        """
 
+        paths1 = self._get_store_paths(store_id1)
+        paths2 = self._get_store_paths(store_id2)
 
-def get_difference_of_paths(store_id1: str, store_id2: str) -> List[str]:
-    """
-    Returns a list of paths that are in first store but not in second.
-    Throws an exception on nix error.
-    """
+        return list(set(paths1) - set(paths2))
 
-    paths1 = _get_store_paths(store_id1)
-    paths2 = _get_store_paths(store_id2)
+    def get_difference_of_package_closures(
+        self, store1: str, package1: str, store2: str, package2: str
+    ) -> List[str]:
+        """
+        Returns a list of paths that are in first package but not in second.
+        Throws an exception on nix error.
+        """
 
-    return list(set(paths1) - set(paths2))
+        paths1 = self._get_package_closure(store1, package1)
+        paths2 = self._get_package_closure(store2, package2)
 
-
-def get_difference_of_package_closures(
-    store1: str, package1: str, store2: str, package2: str
-) -> List[str]:
-    """
-    Returns a list of paths that are in first package but not in second.
-    Throws an exception on nix error.
-    """
-
-    paths1 = _get_package_closure(store1, package1)
-    paths2 = _get_package_closure(store2, package2)
-
-    return list(set(paths1) - set(paths2))
+        return list(set(paths1) - set(paths2))
