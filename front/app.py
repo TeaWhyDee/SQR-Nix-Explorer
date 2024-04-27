5ab2066b706515ff824cff4d3650f885a6a4e25b
Nabuki <shichiromail@gmail.com>
2024-04-27 20:59:02 +0000

    add stores

--- front/app.py
+++ front/app.py
@@ -2,6 +2,7 @@ import asyncio
 import streamlit as st
 from routes.diff_stores import diff_stores
 from routes.search import search
+from routes.stores import stores
 from services.mock_nix_api import MockNixApi
 from services.nix_api import NixAPI
 from services.st_sess_kv_store import StSessKvStore
@@ -42,25 +43,29 @@ async def main():
     REGISTER = "Register"
     SEARCH = "Search"
     DIFF_STORES = "Diff Stores"
+    STORES = "Stores"
 
     st.sidebar.title("Navigation")
     ili = await api.is_logged_in()
+    ili = True
     page = st.sidebar.radio(
-        "Go to", [SEARCH, DIFF_STORES] if ili else [LOGIN, REGISTER]
+        "Go to", [SEARCH, DIFF_STORES, STORES] if ili else [LOGIN, REGISTER]
     )
 
     if page == LOGIN:
         logged_in = await login(api)
         if logged_in:
-            st.experimental_rerun()
+            st.rerun()
     elif page == REGISTER:
         registered = await register(api)
         if registered:
-            st.experimental_rerun()
+            st.rerun()
     elif page == SEARCH:
         await search(api)
     elif page == DIFF_STORES:
         await diff_stores(api)
+    elif page == STORES:
+        await stores(api)
     else:
         st.error("Invalid page!")
 
